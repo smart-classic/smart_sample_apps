5ba4c9351183d30d34c02c6e92f211f3e58056b1
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2010-06-25 13:57:13 -0400

    fixed defaults

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 urlpatterns = patterns('',
     # static
     ## WARNING NOT FOR PRODUCTION
-    (r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/jmandel/Desktop/smart/smart_sample_app/static/'})
+    (r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/static/'%settings.APP_HOME})
 )
