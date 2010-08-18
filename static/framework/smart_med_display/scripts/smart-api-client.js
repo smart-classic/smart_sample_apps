955e50be313cd1cae08c9ae126e1c163d341d6a4
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2010-08-18 14:51:44 -0400

    Dont' refer to global SMART name.

--- static/framework/smart_med_display/scripts/smart-api-client.js
+++ static/framework/smart_med_display/scripts/smart-api-client.js
@@ -27,7 +27,7 @@ var SMART_CLIENT = function(smart_server_origin, frame) {
 					// easier for development, may need some work
 					this.smart_server_origin = event.origin;
 					if (this.smart_server_origin === "null")
-					    this.smart_server_origin = "*";
+						this.smart_server_origin = "*";
 
 					this.receive_setup_message(parsed_message);
 				}
@@ -61,7 +61,7 @@ var SMART_CLIENT = function(smart_server_origin, frame) {
 		this.record_info = message.record_info;
 		var _this = this;
 		this.CAPABILITIES_get(function() {
-			_this.ready_callback(_this.record_info);			
+			_this.ready_callback(_this.record_info);
 		});
 	},
 
@@ -95,10 +95,11 @@ var SMART_CLIENT = function(smart_server_origin, frame) {
 					'type' : 'apicall',
 					'uuid' : call_uuid,
 					'func' : options.url,
-					'method': options.method,
+					'method' : options.method,
 					'params' : options.data,
-					'contentType' : options.contentType || "application/x-www-form-urlencoded"
-					    }), this.smart_server_origin);
+					'contentType' : options.contentType
+							|| "application/x-www-form-urlencoded"
+				}), this.smart_server_origin);
 			}
 
 	var _this = this;
@@ -140,25 +141,27 @@ var SMART_CLIENT = function(smart_server_origin, frame) {
 		};
 
 		// dynamically load any javascript file.
-		load.getScript =  function(filename) {
+		load.getScript = function(filename) {
 			var script = document.createElement('script');
 			script.setAttribute("type", "text/javascript");
 			script.setAttribute("src", filename.url);
 			load.filename = filename;
 			if (typeof script !== "undefined")
-				document.getElementsByTagName("head")[0].appendChild(script); 
+				document.getElementsByTagName("head")[0].appendChild(script);
 			load.tryReady(0);
 		};
 
 		load.tryReady = function(time_elapsed) {
 			// Continually polls to see if jQuery is loaded.
 			if (load.filename()) { // if jQuery isn't loaded yet...
-				if (time_elapsed <= 10000) { // and we havn't given up trying...
+				if (time_elapsed <= 10000) { // and we havn't given up
+					// trying...
 					setTimeout(function() {
 						load.tryReady(time_elapsed + 200);
 					}, 200); // set a timer to check again in 200 ms.
 				} else {
-					alert("Timed out while loading jQuery: " + load.filename.url);
+					alert("Timed out while loading jQuery: "
+							+ load.filename.url);
 				}
 			} else {
 				load.callback();
@@ -168,21 +171,35 @@ var SMART_CLIENT = function(smart_server_origin, frame) {
 		var filenames = [];
 		var need_rest = false;
 
-		var need_jquery = function() { return (typeof (jQuery) === "undefined" || typeof (jQuery.fn) === "undefined");};
-		need_jquery.url =  "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js";
-		
-		var need_ui  = function() {try {return (typeof(jQuery.fn.autocomplete) === "undefined");} catch(e) {return true;}};
+		var need_jquery = function() {
+			return (typeof (jQuery) === "undefined" || typeof (jQuery.fn) === "undefined");
+		};
+		need_jquery.url = "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js";
+
+		var need_ui = function() {
+			try {
+				return (typeof (jQuery.fn.autocomplete) === "undefined");
+			} catch (e) {
+				return true;
+			}
+		};
 		need_ui.url = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.js";
 
-		var need_rdf = function() {try {return (typeof(jQuery.rdf) === "undefined");} catch(e) {return true;}};
+		var need_rdf = function() {
+			try {
+				return (typeof (jQuery.rdf) === "undefined");
+			} catch (e) {
+				return true;
+			}
+		};
 		need_rdf.url = "http://sandbox.smartplatforms.org:8001/framework/smart_med_display/scripts/jquery.rdfquery.core-1.0.js";
-		
-		var funcs = [need_jquery, need_ui, need_rdf];
-		
-		for (var i = 0; i < funcs.length; i++)
+
+		var funcs = [ need_jquery, need_ui, need_rdf ];
+
+		for ( var i = 0; i < funcs.length; i++)
 			if (funcs[i]())
 				filenames.push(funcs[i]);
-		
+
 		load(filenames);
 	};
 
@@ -232,178 +249,198 @@ function randomUUID() {
 
 SMART_CLIENT.prototype.MEDS_get = function(callback) {
 	var _this = this;
-	this.api_call({method: 'GET', 
-		url: "/records/" + SMART.record_info.id + "/medications/", 
-		data: {}},
-	function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				callback(rdf);
-			});
-	
+	this.api_call( {
+		method : 'GET',
+		url : "/records/" + _this.record_info.id + "/medications/",
+		data : {}
+	}, function(contentType, data) {
+		var rdf = _this.process_rdf(contentType, data);
+		callback(rdf);
+	});
+
 };
 
 SMART_CLIENT.prototype.MEDS_get_all = SMART_CLIENT.prototype.MEDS_get;
 
 SMART_CLIENT.prototype.MEDS_post = function(data, callback) {
 	var _this = this;
-	this.api_call({method: 'POST', 
-			url: "/records/" + SMART.record_info.id + "/medications/", 
-				   contentType: 'application/rdf+xml', 
-				   data: data},
-			function(contentType, data) {
-				callback(data);
-			});
+	this.api_call( {
+		method : 'POST',
+		url : "/records/" + _this.record_info.id + "/medications/",
+		contentType : 'application/rdf+xml',
+		data : data
+	}, function(contentType, data) {
+		callback(data);
+	});
 };
 
 SMART_CLIENT.prototype.MEDS_delete = function(callback) {
 	var _this = this;
-	this.api_call({method: 'DELETE', 
-					url: "/records/" + SMART.record_info.id + "/medications/", 
-				   data: {}},
-			function(contentType, data) {
-				callback(data);
-			});
+	this.api_call( {
+		method : 'DELETE',
+		url : "/records/" + _this.record_info.id + "/medications/",
+		data : {}
+	}, function(contentType, data) {
+		callback(data);
+	});
 };
-
 
 SMART_CLIENT.prototype.MED_delete = function(uri, callback) {
 	var _this = this;
-	this.api_call({method: 'DELETE', 
-					url: uri, 
-				   data: {}},
-			function(contentType, data) {
-				callback(data);
-			});
+	this.api_call( {
+		method : 'DELETE',
+		url : uri,
+		data : {}
+	}, function(contentType, data) {
+		callback(data);
+	});
 };
-
 
 SMART_CLIENT.prototype.MED_put = function(data, external_id, callback) {
 	var _this = this;
-	this.api_call({method: 'PUT', 
-		url: "/records/" + SMART.record_info.id + "/medications/external_id/"+external_id, 
-		contentType: 'application/rdf+xml', 
-		data: data},
-	function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				callback(rdf);
-			});
-	
+	this.api_call( {
+		method : 'PUT',
+		url : "/records/" + _this.record_info.id + "/medications/external_id/"
+				+ external_id,
+		contentType : 'application/rdf+xml',
+		data : data
+	}, function(contentType, data) {
+		var rdf = _this.process_rdf(contentType, data);
+		callback(rdf);
+	});
+
 };
 
 SMART_CLIENT.prototype.PROBLEMS_get = function(callback) {
 	var _this = this;
-	this.api_call({method: 'GET', 
-		   url: "/records/" + SMART.record_info.id + "/problems/", 
-		   data: {}},
-	function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				callback(rdf);
-			});
+	this.api_call( {
+		method : 'GET',
+		url : "/records/" + _this.record_info.id + "/problems/",
+		data : {}
+	}, function(contentType, data) {
+		var rdf = _this.process_rdf(contentType, data);
+		callback(rdf);
+	});
 };
 
-SMART_CLIENT.prototype.PROBLEMS_post= function(data, callback) {
+SMART_CLIENT.prototype.PROBLEMS_post = function(data, callback) {
 	var _this = this;
-	this.api_call({method: 'POST', 
-			url: "/records/" + SMART.record_info.id + "/problems/", 
-				   contentType: 'application/rdf+xml', 
-				   data: data},
-			function(contentType, data) {
-				callback(data);
-			});
+	this.api_call( {
+		method : 'POST',
+		url : "/records/" + _this.record_info.id + "/problems/",
+		contentType : 'application/rdf+xml',
+		data : data
+	}, function(contentType, data) {
+		callback(data);
+	});
 };
-
 
 SMART_CLIENT.prototype.PROBLEMS_delete = function(problem_uri, callback) {
 	var _this = this;
-		
-	this.api_call({method: 'DELETE', 
-		   	url: problem_uri, 
-			data: {}
-			},
-			function(contentType, data) {
-				callback(data);
-			});
+
+	this.api_call( {
+		method : 'DELETE',
+		url : problem_uri,
+		data : {}
+	}, function(contentType, data) {
+		callback(data);
+	});
 };
 
 SMART_CLIENT.prototype.PROBLEM_put = function(data, external_id, callback) {
 	var _this = this;
-	this.api_call({method: 'PUT', 
-		url: "/records/" + SMART.record_info.id + "/problems/external_id/"+external_id, 
-		contentType: 'application/rdf+xml', 
-		data: data},
-	function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				callback(rdf);
-			});
+	this.api_call( {
+		method : 'PUT',
+		url : "/records/" + _this.record_info.id + "/problems/external_id/"
+				+ external_id,
+		contentType : 'application/rdf+xml',
+		data : data
+	}, function(contentType, data) {
+		var rdf = _this.process_rdf(contentType, data);
+		callback(rdf);
+	});
 };
 
-
-
-
-
-SMART_CLIENT.prototype.CODING_SYSTEM_get = function(system, query,callback) {
+SMART_CLIENT.prototype.CODING_SYSTEM_get = function(system, query, callback) {
 	var _this = this;
-	this.api_call({method: 'GET', 
-		   url: "/codes/systems/"+system+"/query", 
-		   data: {q : query}},
-	function(contentType, data) {
-			   var js =  JSON.parse(data);
-				callback(js);
-			});
+	this.api_call( {
+		method : 'GET',
+		url : "/codes/systems/" + system + "/query",
+		data : {
+			q : query
+		}
+	}, function(contentType, data) {
+		var js = JSON.parse(data);
+		callback(js);
+	});
 }
 
 SMART_CLIENT.prototype.SPL_get = function(query, callback) {
 	var _this = this;
-	this.api_call({method: 'GET', 
-		   url: "/spl/for_rxnorm/"+query, 
-		   data: {}},
-	function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				callback(rdf);
-			});
+	this.api_call( {
+		method : 'GET',
+		url : "/spl/for_rxnorm/" + query,
+		data : {}
+	}, function(contentType, data) {
+		var rdf = _this.process_rdf(contentType, data);
+		callback(rdf);
+	});
 }
 
 SMART_CLIENT.prototype.CAPABILITIES_get = function(callback) {
 	var _this = this;
-	this.api_call({method: 'GET', 
-		   url: "/capabilities/", 
-		   data: {}},
-	function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				cs = rdf.where("?platform  rdf:type  <http://smartplatforms.org/container>")
-						.where("?platform   <http://smartplatforms.org/capability> ?cap");
-				
-				_this.capabilities = {}
-				for (var i = 0; i < cs.length; i++) {
-					_this.capabilities[cs[i].cap.value._string] = true;
-				}
-				
-				callback(rdf);
-			});
-}
+	this
+			.api_call(
+					{
+						method : 'GET',
+						url : "/capabilities/",
+						data : {}
+					},
+					function(contentType, data) {
+						var rdf = _this.process_rdf(contentType, data);
+						cs = rdf
+								.where(
+										"?platform  rdf:type  <http://smartplatforms.org/container>")
+								.where(
+										"?platform   <http://smartplatforms.org/capability> ?cap");
 
+						_this.capabilities = {}
+						for ( var i = 0; i < cs.length; i++) {
+							_this.capabilities[cs[i].cap.value._string] = true;
+						}
+
+						callback(rdf);
+					});
+}
 
 SMART_CLIENT.prototype.AUTOCOMPLETE_RESOLVER = function(system) {
 	var _this = this;
 	var source = function(request, response) {
 		_this.CODING_SYSTEM_get(system, request.term, function(json) {
-			response(jQuery.map(json, function(item) {return {label: item.full_value, value: item.umls_code};}));
-		}) 
+			response(jQuery.map(json, function(item) {
+				return {
+					label : item.full_value,
+					value : item.umls_code
+				};
+			}));
+		})
 	};
-	
+
 	return source;
 }
 
-SMART_CLIENT.prototype.SPARQL = function(query,callback) {
+SMART_CLIENT.prototype.SPARQL = function(query, callback) {
 	var _this = this;
-	this.api_call({
-		   method: 'GET', 
-		   url: "/records/"+SMART.record_info.id+"/sparql", 
-		   data: {q : query}},
-		   function(contentType, data) {
-				var rdf = _this.process_rdf(contentType, data);
-				callback(rdf);
-			});
+	this.api_call( {
+		method : 'GET',
+		url : "/records/" + _this.record_info.id + "/sparql",
+		data : {
+			q : query
+		}
+	}, function(contentType, data) {
+		var rdf = _this.process_rdf(contentType, data);
+		callback(rdf);
+	});
 };
 
 SMART_CLIENT.prototype.createXMLDocument = function(string) {
@@ -411,16 +448,16 @@ SMART_CLIENT.prototype.createXMLDocument = function(string) {
 };
 
 SMART_CLIENT.prototype.process_rdf = function(contentType, data) {
-	
+
 	if (contentType !== "xml")
 		throw "getRDF expected an XML document... got " + contentType;
 
 	// Get the triples into jquery.rdf
 	var d = this.createXMLDocument(data);
-	
+
 	var rdf = jQuery.rdf();
 	rdf.base("");
-	
+
 	try {
 		rdf.load(d, {});
 		// Load all the namespaces from the xml+rdf into jquery.rdf
@@ -431,12 +468,11 @@ SMART_CLIENT.prototype.process_rdf = function(contentType, data) {
 				rdf.prefix(match[1], a.nodeValue);
 			}
 		}
+	} catch (err) {
 	}
-	catch(err){ }
 
-	rdf.prefix("sp","http://smartplatforms.org/");
-	rdf.prefix("dcterms","http://purl.org/dc/terms/");
+	rdf.prefix("sp", "http://smartplatforms.org/");
+	rdf.prefix("dcterms", "http://purl.org/dc/terms/");
 	// abstract method to instantiate a list of objects from the rdf store.
 	return rdf;
 }
-
