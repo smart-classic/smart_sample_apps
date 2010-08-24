01fa92eb50b46925c24e328962a646e08da3b3c1
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2010-08-24 16:20:49 -0400

    sp:medication has instructions not notes

--- rdf_utils.py
+++ rdf_utils.py
@@ -91,7 +91,7 @@ def get_medication_uris(g):
 def get_medication_model(g,med_uri):
     properties = [NS['dcterms']['title'], 
                   NS['med']['drug'],
-                  NS['med']['notes'],
+                  NS['med']['instructions'],
                   NS['med']['strength'],
                   NS['med']['strengthUnits'],
                   NS['med']['dose'],
