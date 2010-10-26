d1fa8d37b3318b2a80da2853778320de1fa3c4a0
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2010-10-26 11:29:17 -0700

    Use jschannel for postMessage communication

--- static/framework/smart/scripts/smart-api-client.js
+++ static/framework/smart/scripts/smart-api-client.js
@@ -1,219 +1,91 @@
 /*
  * SMArt API client
  *
- * randomUUID is included at the bottom from:
- * http://ajaxian.com/archives/uuid-generator-in-javascript
- * 
  * Josh Mandel
  * Ben Adida
  */
 
 var SMART_CLIENT = function(smart_server_origin, frame) {
+    var debug = true;
 	var _this = this;
-
-	this.init = function() {
-		// the init code has moved down here...
+	this.message_receivers = {};
+	this.send_ready_message = function(ready_callback) {
 		this.smart_server_origin = smart_server_origin;
 		this.frame = frame;
+		var _this = this;
 
-		this.active_calls = {};
-
-		// register the message receiver
-		// wrap in a function because of "this" binding
-
-		window.addEventListener("message", function(message) {
-			_this.receive_message(message);
-		}, false);
-
-	}
-	
-	this.init();
-
-	this.send_ready_message = function(ready_callback) {
-		loadJQuery(function() {
-			var call_uuid = randomUUID();
-			_this.active_calls[call_uuid] = ready_callback;
-			
-			// FIXME: we are not setting a destination constraint here
-			_this.frame.postMessage(
-					JSON.stringify({
-							'type' : 'ready',
-							'call_uuid': call_uuid}), 
-					'*');
+		loadDependencies(function() {
+			_this.channel = Channel.build({window: frame, origin: "*", scope:"not_ready", debugOutput: debug});
+			_this.ready_callback = ready_callback;
+			_this.channel.call({method: "ready", 
+				            success: _this.callback(_this.received_setup)
+				           });
 		});
 	};
 
-    // a message received from the SMArt container
-	this.receive_message = function(event) {
-		// only listen for events from the SMArt container origin
-		if (this.smart_server_origin != null
-				&& event.origin != this.smart_server_origin)
-			return;
-
-		// parse message
-		var parsed_message = JSON.parse(event.data);
-
-		// setup message with credentials and initial data
-		if (parsed_message.type == 'setup') {
-			// FIXME: for now we are binding this client when it
-			// receives the setup message.
-			// easier for development, may need some work
-			this.smart_server_origin = event.origin;
-			if (this.smart_server_origin === "null")
-				this.smart_server_origin = "*";
-
-			this.receive_setup_message(parsed_message);
-		}
-
-		// api return
-		if (parsed_message.type == 'apireturn') {
-			this.receive_apireturn_message(parsed_message);
-		}
-		
-		// api return
-		if (parsed_message.type == 'activityreturn') {
-			this.receive_activityreturn_message(parsed_message);
-		}
-		// api return
-		if (parsed_message.type == 'activitybackground') {
-			this.message_receivers.background();
-		}
-		// api return
-		if (parsed_message.type == 'activityforeground') {
-			this.message_receivers.foreground();
-		}
+	this.callback = function(f) {
+	    var _this = this;
+	    return function() {return f.apply(_this, arguments);};
 	},
 
-	this.receive_setup_message = function(message) {
-		var ready_callback = this.active_calls[message.call_uuid];
-		if (!ready_callback)
-			return;
+        this.received_setup = function(message) {
+	    this.channel.destroy();
+	    this.channel = Channel.build({window: frame, origin: "*", scope: message.activity_id, debugOutput: debug});
+
+	    this.channel.bind("activityforeground", this.callback(function() {this.message_receivers.foreground();}));
+	    this.channel.bind("activitybackground", this.callback(function() {this.message_receivers.background();}));
 		
-		this.credentials = message.credentials;
-		this.record_info = message.record_info;
-		this.activity_id = message.activity_id;
-		this.ready_data = message.ready_data;
+	    this.credentials = message.credentials;
+	    this.record_info = message.record_info;
+	    this.ready_data = message.ready_data;
 		
-		var _this = this;
-		this.CAPABILITIES_get(function() {
-			ready_callback(_this.record_info, _this.ready_data);
+	    var _this = this;
+	    this.CAPABILITIES_get(function() {
+		    _this.ready_callback(_this.record_info, _this.ready_data);
 		});
+	    
 	};
 
-	this.receive_apireturn_message = function(message) {
-		var callback = this.active_calls[message.call_uuid];
-
-		if (callback == null) {
-			return;
-		}
-
-		// unwrap the content-type
-		callback(message.content_type, message.payload);
-
-		// FIXME: we should parse payload XML and JSON here, depending on
-		// content type.
-
-		// clear out the callback
-		this.active_calls[message.call_uuid] = null;
-	};
-
-	this.receive_activityreturn_message = function(message) {
-		this.receive_apireturn_message(message);
-	};
-
-	
-	// generic api_call function
-			// api calls have a function name and named variables
-			// we have to track the calls because asynchronicity might
-			// make the results come back in a different order
 	this.api_call = function(options, callback) {
-		var call_uuid = randomUUID();
-		this.active_calls[call_uuid] = callback;
-
-		this.frame.postMessage(JSON.stringify( {
-			'activity_id': this.activity_id,
-			'type' : 'apicall',
-			'call_uuid' : call_uuid,
+	    this.channel.call({method: "api_call",
+			       params: 
+			{
 			'func' : options.url,
 			'method' : options.method,
 			'params' : options.data,
-			'contentType' : options.contentType
-					|| "application/x-www-form-urlencoded"
-		}), this.smart_server_origin);
+			'contentType' : options.contentType || "application/x-www-form-urlencoded"
+			},
+			success: function(r) { callback(r.contentType, r.data); }
+		});
 	};
 
 	this.start_activity = function(activity_name, app_id, ready_data, callback) {
 		if (arguments.length < 4) {
 			return this.start_activity(activity_name, null,  app_id, ready_data);
 		}
-		
-		var call_uuid = randomUUID();
-		this.active_calls[call_uuid] = callback;
 
-		this.frame.postMessage(JSON.stringify( {
-			'activity_id': this.activity_id,
-			'type' : 'start_activity',
-			'call_uuid' : call_uuid,
+		this.channel.call({method: "start_activity",
+			       params: {
 			'name' : activity_name,
 			'app' : app_id,
 			'ready_data': ready_data
-		}), this.smart_server_origin);
+			},
+				   success: callback||function(){}
+		});
 	};
 
 	
 	this.end_activity = function(response, callback) {
-		var call_uuid = randomUUID();
-		this.active_calls[call_uuid] = null;
-
-		this.frame.postMessage(JSON.stringify( {
-			'activity_id': this.activity_id,
-			'type' : 'end_activity',
-			'call_uuid' : call_uuid,
+	    this.channel.call({method: "end_activity",
+			       params: {
 			'response': response
-		}), this.smart_server_origin);
+		    },
+			       success: callback||function(){}
+		});
 	};
 
 
 
-/*
- * randomUUID.js - Version 1.0
- * 
- * Copyright 2008, Robert Kieffer
- * 
- * This software is made available under the terms of the Open Software License
- * v3.0 (available here: http://www.opensource.org/licenses/osl-3.0.php )
- * 
- * The latest version of this file can be found at:
- * http://www.broofa.com/Tools/randomUUID.js
- * 
- * For more information, or to comment on this, please go to:
- * http://www.broofa.com/blog/?p=151
- */
-
-/**
- * Create and return a "version 4" RFC-4122 UUID string.
- */
-function randomUUID() {
-	var s = [], itoh = '0123456789ABCDEF';
-	// Make array of random hex digits. The UUID only has 32 digits in it, but
-	// we
-	// allocate an extra items to make room for the '-'s we'll be inserting.
-	for ( var i = 0; i < 36; i++)
-		s[i] = Math.floor(Math.random() * 0x10);
-	// Conform to RFC-4122, section 4.4
-	s[14] = 4; // Set 4 high bits of time_high field to version
-	s[19] = (s[19] & 0x3) | 0x8; // Specify 2 high bits of clock sequence
-	// Convert to hex chars
-	for ( var i = 0; i < 36; i++)
-		s[i] = itoh[s[i]];
-	// Insert '-'s
-	s[8] = s[13] = s[18] = s[23] = '-';
-	return s.join('');
-}
-
-/**
- * end of randomUUID
- */
 
 SMART_CLIENT.prototype.MEDS_get = function(callback) {
 	var _this = this;
@@ -588,7 +460,7 @@ SMART_CLIENT.prototype.process_rdf = function(contentType, data) {
 }
 
 
-var loadJQuery = function(callback) {
+var loadDependencies = function(callback) {
 	var load = function(filenames) {
 		load.getScripts(filenames);
 	}
@@ -660,7 +532,18 @@ var loadJQuery = function(callback) {
 	};
 	need_rdf.url = "http://sample-apps.smartplatforms.org:80/framework/smart/scripts/jquery.rdfquery.core-1.0.js";
 
-	var funcs = [ need_jquery, need_ui, need_rdf ];
+
+	var need_jschannel = function() {
+		try {
+			return (typeof (Channel.build) !== "function");
+		} catch (e) {
+			return true;
+		}
+	};
+
+	need_jschannel.url = "http://github.com/mozilla/jschannel/raw/master/src/jschannel.js";
+
+	var funcs = [ need_jquery, need_ui, need_rdf, need_jschannel ];
 
 	for ( var i = 0; i < funcs.length; i++)
 		if (funcs[i]())
--- static/framework/smart/smart.js
+++ static/framework/smart/smart.js
@@ -7,6 +7,7 @@ steal.plugins(
 	      'jquery/dom/form_params')               // form data helper
 	      .then('scripts/class')
 	      .then('scripts/json2')
+	      .then('scripts/jschannel')
 	      .then('scripts/smart-api-client')
 	      .then('scripts/jquery.hotkeys')
 	      .then('scripts/jquery.addons')
