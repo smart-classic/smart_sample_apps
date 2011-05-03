e3af6643b778724e6a773b3be8472dae3ad0cf06
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-05-03 12:20:53 -0700

    Date parsing for iOS / android browsers

--- static/framework/mobile_pedibp/util.js
+++ static/framework/mobile_pedibp/util.js
@@ -1,16 +1,14 @@
 var years_apart = function(d1, d2) {
-
- return (parse_date(d1).getTime()-parse_date(d2).getTime())/ (1000 * 60 * 60 * 24 * 365)
+    return (1.0*parse_date(d1).getTime()-parse_date(d2).getTime())/ (1000 * 60 * 60 * 24 * 365);
 
 };
 	var parse_date = function(d) {
-            var dp = new Date(d);
-            if (isNaN(dp)) { // IE Date constructor doesn't parse ISO-8601 -JCM
-               d = d.split("-");
-               var dp = new Date();
-               dp.setFullYear(d[0], d[1]-1, d[2]);
-            }
-	    return dp;
+	    ret = [0,0,0,0,0,0];
+	    var arr = d.match(/\d+/g);
+	    for (var i = 0; i < arr.length; i++)
+		ret[i] = arr[i];
+	    ret =  new Date(ret[0], ret[1]-1, ret[2], ret[3], ret[4], ret[5]);
+	    return ret;
 	};
 
 	  
