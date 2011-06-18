9d0ac805d28beef4b7a279fb1fe60c7cf82127bc
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2011-06-17 21:31:50 -0400

    Fixes date parsing bug on Safari (and possibly IE)

--- static/framework/pedi_bpc/js/util.js
+++ static/framework/pedi_bpc/js/util.js
@@ -4,17 +4,18 @@ var years_apart = function(d1, d2) {
  //console.log("NEWD");
  //console.log(parse_date(d1));
  //console.log(parse_date(d2).getTime());
-
- return (parse_date(d1).getTime()-parse_date(d2).getTime())/ (1000 * 60 * 60 * 24 * 365)
-
+	var res = (parse_date(d1).getTime()-parse_date(d2).getTime())/ (1000 * 60 * 60 * 24 * 365); 
+	//console.log (d1 + " : " + d2 + " -> " +  res);
+	return res;
 };
         
 	var parse_date = function(d) {
             var dp = new Date(d);
             if (isNaN(dp)) { // IE Date constructor doesn't parse ISO-8601 -JCM
                d = d.split("-");
-               var dp = new Date();
-               dp.setFullYear(d[0], d[1]-1, d[2]);
+			   d[2] = (d[2].split("T"))[0];
+               var dp = new Date(); 
+               dp.setFullYear(d[0], d[1], d[2]);
             }
 	    return dp;
 	};
