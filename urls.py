22882777034f7df8c0ac662ff2a36e2d6c9fe184
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2011-07-12 23:53:39 -0400

    Removed the surescript_connector from urls.py

--- urls.py
+++ urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    # static
-    (r'^surescripts_connector/', include('smart_sample_apps.surescripts_connector.urls')),
     (r'^webhook/', include('smart_sample_apps.webhook.urls')),
     (r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/static/'%settings.APP_HOME})
 )
