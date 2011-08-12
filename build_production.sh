215f1211a9a3ee3c40762df8f97c65facfe4660d
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-08-11 18:20:35 -0700

    Updated build_production script (parallel builds)

--- build_production.sh
+++ build_production.sh
@@ -7,18 +7,17 @@ cat ../../jquery/jquery.js jquery-ui.js  jquery.rdfquery.core-1.0.js jschannel.j
 
 cd ../..
 
-./js med_list/scripts/build.js
-sed -i 's/,development/,production/g' med_list/index.html
 
-./js med_calendar/scripts/build.js
-sed -i 's/,development/,production/g' med_calendar/index.html
+(./js steal/buildjs med_list/index.html) & \
+(./js steal/buildjs problem_list/index.html) & \
+(./js steal/buildjs med_calendar/index.html) & \
+(./js steal/buildjs api_playground/index.html) & \
+(./js steal/buildjs med_adherence/index.html)
 
-./js problem_list/scripts/build.js
+wait
+
 sed -i 's/,development/,production/g' problem_list/index.html
-
-./js api_playground/scripts/build.js
+sed -i 's/,development/,production/g' med_list/index.html
+sed -i 's/,development/,production/g' med_calendar/index.html
 sed -i 's/,development/,production/g' api_playground/index.html
-
-./js med_adherence/scripts/build.js
 sed -i 's/,development/,production/g' med_adherence/index.html
-
