2cebeae7db2c640e427fd264ce392cf48453eff5
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-09-30 10:04:13 -0700

    Removed webhook-depdendent apps (allergy_check)

--- urls.py
+++ urls.py
@@ -3,6 +3,5 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    (r'^webhook/', include('smart_sample_apps.webhook.urls')),
     (r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/static/'%settings.APP_HOME})
 )
