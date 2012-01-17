31499412db300a55e558c25fd0b1ba1249b5befa
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-01-18 00:40:23 +0200

    Updates the Direct apps to make the get_all_manifests call through the SMART client

--- direct_apps/utilities.py
+++ direct_apps/utilities.py
@@ -1,18 +1,11 @@
 import json
-import urllib2
 
 from smart_client.smart import SmartClient
 from settings import PROXY_OAUTH, PROXY_PARAMS
 
 def get_app_manifests():
-        # Now process the request
-        #smart_client = SmartClient(PROXY_OAUTH['consumer_key'], PROXY_PARAMS, PROXY_OAUTH, None)
-        #res = smart_client.get("/apps/manifests/")
-        #apps = json.loads(res.body)
-        #apps = sorted(apps, key=lambda app: app['name'])
-        #return json.dumps(apps, indent=4)
-        
-        s = urllib2.urlopen("http://direct.smartplatforms.org:7000/apps/manifests/")
-        res = s.read()
-        s.close()
-        return res
+        smart_client = SmartClient(PROXY_OAUTH['consumer_key'], PROXY_PARAMS, PROXY_OAUTH, None)
+        res = smart_client.get("/apps/manifests/")
+        apps = json.loads(res.body)
+        apps = sorted(apps, key=lambda app: app['name'])
+        return json.dumps(apps, indent=4)
