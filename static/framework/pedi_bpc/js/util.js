950ccfd7833df4119f40ba88c3d55a9456501419
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-03-21 11:00:26 -0400

    Fixes setFullYear method call in the BPC app

--- static/framework/pedi_bpc/js/util.js
+++ static/framework/pedi_bpc/js/util.js
@@ -14,8 +14,12 @@ var years_apart = function(d1, d2) {
             if (isNaN(dp)) { // IE Date constructor doesn't parse ISO-8601 -JCM
                d = d.split("-");
                d[2] = (d[2].split("T"))[0];
-               dp = new Date(); 
-               dp.setFullYear(d[0], d[1], d[2]);
+               dp = new Date();
+               // The month is indexed from 0 based on the Java Script specifications 
+               // for the setFullYear method while the day is indexed from 1
+               // (Does not make sense, but it is what it is!). So we substract 1
+               // from the month to confirm to this spec.
+               dp.setFullYear(d[0], d[1]-1, d[2]);
             }
         return dp;
     };
