3977651302a4213277869c7986e7bccbb57ceb8a
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-04-16 16:53:07 -0400

    Fix for the closre in the connect client

--- static/framework/smart/scripts/build_smart_api_client.sh
+++ static/framework/smart/scripts/build_smart_api_client.sh
@@ -8,9 +8,9 @@ python build.py > smart-temp.js
 
 echo "(function(window){" >> smart-api-client.js
 
-cat jquery.min.js  jquery.rdfquery.core-1.0.js  >> smart-api-client.js
-echo "\nvar $ = window.jQuery.noConflict(true), jQuery = $;" >> smart-api-client.js
-cat jschannel.js smart-temp.js  >> smart-api-client.js
+cat jquery.min.js >> smart-api-client.js
+echo "var $ = window.jQuery.noConflict(true), jQuery = $;" >> smart-api-client.js
+cat jquery.rdfquery.core-1.0.js jschannel.js smart-temp.js  >> smart-api-client.js
 
 echo "})(window);" >> smart-api-client.js
 
