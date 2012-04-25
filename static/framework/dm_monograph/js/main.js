f9d925e803a4ff1f546ce171882d0b02097dd90a
Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
2012-04-25 13:28:15 -0400

    Insert all last known values, units, and dates

--- static/framework/dm_monograph/js/main.js
+++ static/framework/dm_monograph/js/main.js
@@ -108,22 +108,34 @@ var VITAL_SIGNS_get = function(){
            .where('?vital_id   sp:bloodPressure ?bn2')
            .where('?vital_id   dcterms:date     ?date')
            .each(function(){
-             if (type === 'systolic') bp_array = pt.sbp_array;
-             else bp_array = pt.dbp_array;
+             if (type === 'systolic') {
+               bp_array = pt.sbp_array;
+               bp_latest = pt.sbp_latest;
+             }
+             else {
+               bp_array = pt.dbp_array;
+               bp_latest = pt.dbp_latest;
+             }
 
              bp_array.push([
                new XDate(this.date.value).valueOf(),
-               Number(this.value.value)
+               Number(this.value.value),
+               this.unit.value
              ])
            })
+
+           // FIXME: DRY
+           pt.bp_array = _(pt.bp_array).sortBy(function(item){ return item[0]; })
+           pt.bp_latest = _(pt.bp_array).last() || null
         }
 
         // ruby style!
         pt.dbp_array = [];
         pt.sbp_array = [];
+        pt.dbp_latest = null;
+        pt.sbp_latest = null;
         _get_bps('systolic');
         _get_bps('diastolic')
-
         dfd.resolve();
       })
       .error(error_callback);
@@ -134,13 +146,16 @@ var LAB_RESULTS_get = function(){
   return $.Deferred(function(dfd){
     SMART.LAB_RESULTS_get()
       .success(function(r){
+        //
+        // FIXME: DRY (obviously)
+        //
+
         // LDL Codes
         //
         // LOINC Code, Long name, Short Name, class, rank # of 2000
         // 13457-7	Cholesterol in LDL [Mass/volume] in Serum or Plasma by calculation	LDLc SerPl Calc-mCnc	CHEM	63
         // 2089-1	Cholesterol in LDL [Mass/volume] in Serum or Plasma	LDLc SerPl-mCnc	CHEM	92
         // 18262-6	Cholesterol in LDL [Mass/volume] in Serum or Plasma by Direct assay	LDLc SerPl Direct Assay-mCnc	CHEM	249
-
         // FIXME: ONLY top LDL code!!
         pt.ldl_array = [];
         r.graph
@@ -164,23 +179,23 @@ var LAB_RESULTS_get = function(){
            var d = new XDate(this.date.value)
            d.addYears(3, true);
 
+           // array is [js timestamp, value as number, unit as string]
+           // flot uses js timestamps on the x axis, we convert them to human-readable strings later
            pt.ldl_array.push([
-             d.valueOf(),
-             Number(this.value.value)
+              d.valueOf(),
+              Number(this.value.value),
+              this.unit.value
            ])
          })
 
-         pt.ldl_latest = _.chain(pt.ldl_array)
-                          .sortBy(function(item){ return item[0]; })
-                          .last()
-                          .value() || null
+         pt.ldl_array = _(pt.ldl_array).sortBy(function(item){ return item[0]; })
+         pt.ldl_latest = _(pt.ldl_array).last() || null
 
          // A1C Codes
          //
          // LOINC Code, Long name, Short Name, class, rank # of 2000
          // 4548-4,Hemoglobin A1c/Hemoglobin.total in Blood,Hgb A1c MFr Bld,HEM/BC,81
          // 17856-6,Hemoglobin A1c/Hemoglobin.total in Blood by HPLC,Hgb A1c MFr Bld HPLC,HEM/BC,215
-
          // FIXME: ONLY top A1c code!!
          pt.a1c_array = [];
          r.graph
@@ -206,14 +221,13 @@ var LAB_RESULTS_get = function(){
 
             pt.a1c_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.a1c_latest = _.chain(pt.a1c_array)
-                           .sortBy(function(item){ return item[0]; })
-                           .last()
-                           .value() || null
+          pt.a1c_array = _(pt.a1c_array).sortBy(function(item){ return item[0]; })
+          pt.a1c_latest = _(pt.a1c_array).last() || null
 
 
           // Ur Tp
@@ -222,7 +236,6 @@ var LAB_RESULTS_get = function(){
           // 2888-6,Protein [Mass/volume] in Urine,Prot Ur-mCnc,UA,292
           // 35663-4,Protein [Mass/volume] in unspecified time Urine,Prot ?Tm Ur-mCnc,UA,635
           // 21482-5,Protein [Mass/volume] in 24 hour Urine,Prot 24H Ur-mCnc,CHEM,1696
-
           // FIXME: ONLY top code!!
           pt.ur_tp_array = [];
           r.graph
@@ -242,22 +255,19 @@ var LAB_RESULTS_get = function(){
            .where('?bn4 sp:startDate          ?date')
            .each(function(){
              pt.ur_tp_array.push([
-               new XDate(this.date.value).valueOf(),
-               Number(this.value.value)
+                new XDate(this.date.value).valueOf(),
+                Number(this.value.value),
+                this.unit.value
              ])
            })
 
-           pt.ur_tp_latest = _.chain(pt.ur_tp_array)
-                              .sortBy(function(item){ return item[0]; })
-                              .last()
-                              .value() || null
-
+           pt.ur_tp_array = _(pt.ur_tp_array).sortBy(function(item){ return item[0]; })
+           pt.ur_tp_latest = _(pt.ur_tp_array).last() || null
 
          // Microalbumin/Creatinine [Mass ratio] in Urine
          //
          // 14959-1,Microalbumin/Creatinine [Mass ratio] in Urine,Microalbumin/Creat Ur-mRto,CHEM,212
          // 14958-3,Microalbumin/Creatinine [Mass ratio] in 24 hour Urine,Microalbumin/Creat 24H Ur-mRto,CHEM,1979
-         
          // FIXME: ONLY top code!!
          pt.micro_alb_cre_ratio_array = [];
          r.graph
@@ -283,15 +293,13 @@ var LAB_RESULTS_get = function(){
 
             pt.micro_alb_cre_ratio_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.micro_alb_cre_ratio_latest = _.chain(pt.micro_alb_cre_ratio_array)
-                                           .sortBy(function(item){ return item[0]; })
-                                           .last()
-                                           .value() || null
-
+          pt.micro_alb_cre_ratio_array = _(pt.micro_alb_cre_ratio_array).sortBy(function(item){ return item[0]; })
+          pt.micro_alb_cre_ratio_latest = _(pt.micro_alb_cre_ratio_array).last() || null
 
          // Aspartate aminotransferase / SGOT / AST
          //
@@ -299,7 +307,6 @@ var LAB_RESULTS_get = function(){
          //
          // LOINC Code, Long name, Short Name, class, rank # of 2000
          // 1920-8,Aspartate aminotransferase [Enzymatic activity/volume] in Serum or Plasma,AST SerPl-cCnc,CHEM,19
-
          pt.sgot_array = [];
          r.graph
           .prefix('rdf',     'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
@@ -324,16 +331,15 @@ var LAB_RESULTS_get = function(){
 
             pt.sgot_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.sgot_array_latest = _.chain(pt.sgot_array)
-                                   .sortBy(function(item){ return item[0]; })
-                                   .last()
-                                   .value() || null
+          pt.sgot_array = _(pt.sgot_array).sortBy(function(item){ return item[0]; })
+          pt.sgot_latest = _(pt.sgot_array).last() || null
 
-         // Cholesterol (total): only 1 code!!
+         // Cholesterol (total): only 1 code!! Yay!
          //
          // 2093-3,Cholesterol [Mass/volume] in Serum or Plasma,Cholest SerPl-mCnc,CHEM,32
          pt.cholesterol_total_array = [];
@@ -360,22 +366,19 @@ var LAB_RESULTS_get = function(){
 
             pt.cholesterol_total_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.cholesterol_total_latest = _.chain(pt.cholesterol_total_array)
-                                   .sortBy(function(item){ return item[0]; })
-                                   .last()
-                                   .value() || null
+          pt.cholesterol_total_array = _(pt.cholesterol_total_array).sortBy(function(item){ return item[0]; })
+          pt.cholesterol_total_latest = _(pt.cholesterol_total_array).last() || null
 
          // Tri
          //
          // 2571-8,Triglyceride [Mass/volume] in Serum or Plasma,Trigl SerPl-mCnc,CHEM,36
          // 3043-7,Triglyceride [Mass/volume] in Blood,Trigl Bld-mCnc,CHEM,1592
-         
          // fixme only 1 code!
-
          pt.triglyceride_array = [];
          r.graph
           .prefix('rdf',     'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
@@ -400,18 +403,16 @@ var LAB_RESULTS_get = function(){
 
             pt.triglyceride_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.triglyceride_latest = _.chain(pt.triglyceride_array)
-                                   .sortBy(function(item){ return item[0]; })
-                                   .last()
-                                   .value() || null
+          pt.triglyceride_array = _(pt.triglyceride_array).sortBy(function(item){ return item[0]; })
+          pt.triglyceride_latest = _(pt.triglyceride_array).last() || null
 
          // HDL
          // 2085-9,Cholesterol in HDL [Mass/volume] in Serum or Plasma,HDLc SerPl-mCnc,CHEM,38
-
          pt.hdl_array = [];
          r.graph
           .prefix('rdf',     'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
@@ -436,20 +437,18 @@ var LAB_RESULTS_get = function(){
 
             pt.hdl_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.hdl_latest = _.chain(pt.hdl_array)
-                                    .sortBy(function(item){ return item[0]; })
-                                    .last()
-                                    .value() || null
+          pt.hdl_array = _(pt.hdl_array).sortBy(function(item){ return item[0]; })
+          pt.hdl_latest = _(pt.hdl_array).last() || null
 
          // BUN
          //
          // 3094-0,Urea nitrogen [Mass/volume] in Serum or Plasma,BUN SerPl-mCnc,CHEM,6
          // 6299-2,Urea nitrogen [Mass/volume] in Blood,BUN Bld-mCnc,CHEM,288
-
          // fixme only top code
          pt.bun_array = [];
          r.graph
@@ -475,20 +474,18 @@ var LAB_RESULTS_get = function(){
 
             pt.bun_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.bun_latest = _.chain(pt.bun_latest)
-                                    .sortBy(function(item){ return item[0]; })
-                                    .last()
-                                    .value() || null
+          pt.bun_array = _(pt.bun_array).sortBy(function(item){ return item[0]; })
+          pt.bun_latest = _(pt.bun_array).last() || null
 
          // Cre
          //
          // 2160-0,Creatinine [Mass/volume] in Serum or Plasma,Creat SerPl-mCnc,CHEM,1
          // 38483-4,Creatinine [Mass/volume] in Blood,Creat Bld-mCnc,CHEM,283
-
          // fixme only top code
          pt.creatinine_array = [];
          r.graph
@@ -514,20 +511,18 @@ var LAB_RESULTS_get = function(){
 
             pt.creatinine_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.creatinine_latest = _.chain(pt.creatinine_array)
-                                   .sortBy(function(item){ return item[0]; })
-                                   .last()
-                                   .value() || null
+          pt.creatinine_array = _(pt.creatinine_array).sortBy(function(item){ return item[0]; })
+          pt.creatinine_latest = _(pt.creatinine_array).last() || null
 
 
          // Glu
          // 2345-7,Glucose [Mass/volume] in Serum or Plasma,Glucose SerPl-mCnc,CHEM,4
          // 2339-0,Glucose [Mass/volume] in Blood,Glucose Bld-mCnc,CHEM,13
-
          // fixme only top code
          pt.glucose_array = [];
          r.graph
@@ -553,20 +548,18 @@ var LAB_RESULTS_get = function(){
 
             pt.glucose_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.glucose_latest = _.chain(pt.glucose_array)
-                                   .sortBy(function(item){ return item[0]; })
-                                   .last()
-                                   .value() || null
+          pt.glucose_array = _(pt.glucose_array).sortBy(function(item){ return item[0]; })
+          pt.glucose_latest = _(pt.glucose_array).last() || null
 
          // BUN
          //
          // 3094-0,Urea nitrogen [Mass/volume] in Serum or Plasma,BUN SerPl-mCnc,CHEM,6
          // 6299-2,Urea nitrogen [Mass/volume] in Blood,BUN Bld-mCnc,CHEM,288
-
          // fixme only top code
          pt.bun_array = [];
          r.graph
@@ -592,16 +585,15 @@ var LAB_RESULTS_get = function(){
 
             pt.bun_array.push([
               d.valueOf(),
-              Number(this.value.value)
+              Number(this.value.value),
+              this.unit.value
             ])
           })
 
-          pt.bun_latest = _.chain(pt.bun_array)
-                                   .sortBy(function(item){ return item[0]; })
-                                   .last()
-                                   .value() || null
+          pt.bun_array = _(pt.bun_array).sortBy(function(item){ return item[0]; })
+          pt.bun_latest = _(pt.bun_array).last() || null
 
-
+         // resolved!
          dfd.resolve();
       })
       .error(error_callback);
@@ -630,6 +622,59 @@ SMART.ready(function(){
     var b = new XDate(pt.bday)
     $('#age').text(Math.round(b.diffYears(new XDate())));
     $('#gender').text(pt.gender[0])
+
+
+    // insert data into html
+    // last known values (all arrays sorted by ascending dates)
+    // FIXME: DRY
+    $('#ur_tp_latest_date').text(pt.ur_tp_latest ? new XDate(pt.ur_tp_latest[0]).toString('MM/dd/yy') : null)
+    $('#ur_tp_latest_val') .text(pt.ur_tp_latest ? pt.ur_tp_latest[1] : null)
+    $('#ur_tp_latest_unit').text(pt.ur_tp_latest ? pt.ur_tp_latest[2] : null)
+
+    $('#micro_alb_cre_ratio_latest_date').text(pt.micro_alb_cre_ratio_latest ? new XDate(pt.micro_alb_cre_ratio_latest[0]).toString('MM/dd/yy') : null)
+    $('#micro_alb_cre_ratio_latest_val') .text(pt.micro_alb_cre_ratio_latest ? pt.micro_alb_cre_ratio_latest[1] : null)
+    $('#micro_alb_cre_ratio_latest_unit').text(pt.micro_alb_cre_ratio_latest ? pt.micro_alb_cre_ratio_latest[2] : null)
+
+    $('#sgot_latest_date').text(pt.sgot_latest ? new XDate(pt.sgot_latest[0]).toString('MM/dd/yy') : null)
+    $('#sgot_latest_val') .text(pt.sgot_latest ? pt.sgot_latest[1] : null)
+    $('#sgot_latest_unit').text(pt.sgot_latest ? pt.sgot_latest[2] : null)
+
+    $('#cholesterol_total_latest_date').text(pt.cholesterol_total_latest ? new XDate(pt.cholesterol_total_latest[0]).toString('MM/dd/yy') : null)
+    $('#cholesterol_total_latest_val') .text(pt.cholesterol_total_latest ? pt.cholesterol_total_latest[1] : null)
+    $('#cholesterol_total_latest_unit').text(pt.cholesterol_total_latest ? pt.cholesterol_total_latest[2] : null)
+
+    $('#triglyceride_latest_date').text(pt.triglyceride_latest ? new XDate(pt.triglyceride_latest[0]).toString('MM/dd/yy') : null)
+    $('#triglyceride_latest_val') .text(pt.triglyceride_latest ? pt.triglyceride_latest[1] : null)
+    $('#triglyceride_latest_unit').text(pt.triglyceride_latest ? pt.triglyceride_latest[2] : null)
+
+    $('#hdl_latest_date').text(pt.hdl_latest ? new XDate(pt.hdl_latest[0]).toString('MM/dd/yy') : null)
+    $('#hdl_latest_val') .text(pt.hdl_latest ? pt.hdl_latest[1] : null)
+    $('#hdl_latest_unit').text(pt.hdl_latest ? pt.hdl_latest[2] : null)
+
+    $('#ldl_latest_date').text(pt.ldl_latest ? new XDate(pt.ldl_latest[0]).toString('MM/dd/yy') : null)
+    $('#ldl_latest_val') .text(pt.ldl_latest ? pt.ldl_latest[1] : null)
+    $('#ldl_latest_unit').text(pt.ldl_latest ? pt.ldl_latest[2] : null)
+
+    $('#bun_latest_date').text(pt.bun_latest ? new XDate(pt.bun_latest[0]).toString('MM/dd/yy') : null)
+    $('#bun_latest_val') .text(pt.bun_latest ? pt.bun_latest[1] : null)
+    $('#bun_latest_unit').text(pt.bun_latest ? pt.bun_latest[2] : null)
+
+    $('#creatinine_latest_date').text(pt.creatinine_latest ? new XDate(pt.creatinine_latest[0]).toString('MM/dd/yy') : null)
+    $('#creatinine_latest_val') .text(pt.creatinine_latest ? pt.creatinine_latest[1] : null)
+    $('#creatinine_latest_unit').text(pt.creatinine_latest ? pt.creatinine_latest[2] : null)
+
+    $('#glucose_latest_date').text(pt.glucose_latest ? new XDate(pt.glucose_latest[0]).toString('MM/dd/yy') : null)
+    $('#glucose_latest_val') .text(pt.glucose_latest ? pt.glucose_latest[1] : null)
+    $('#glucose_latest_unit').text(pt.glucose_latest ? pt.glucose_latest[2] : null)
+
+    $('#a1c_latest_date').text(pt.a1c_latest ? new XDate(pt.a1c_latest[0]).toString('MM/dd/yy') : null)
+    $('#a1c_latest_val') .text(pt.a1c_latest ? pt.a1c_latest[1] : null)
+    $('#a1c_latest_unit').text(pt.a1c_latest ? pt.a1c_latest[2] : null)
+
+
+    //
+    // flot
+    //
 
     // testing flot
     var x_min = new XDate('2010').valueOf();
