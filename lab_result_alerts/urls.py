b0a917082be45ab40376b4acc6c2ab641068a595
Josh Mandel <Joshua.Mandel@childrens.harvard.edu>
2012-08-20 14:54:05 -0700

    Remove old admin, alert apps

--- lab_result_alerts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-
-urlpatterns = patterns('',
-    (r'^(?P<path>.*)$', 'django.views.static.serve', 
-     {'document_root': '%s/lab_result_alerts/static'%settings.APP_HOME}),
-)
