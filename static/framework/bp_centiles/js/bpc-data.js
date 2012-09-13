9e5e45f6f1fff29ca884a9228d460fe21a626fd2
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-09-14 00:33:18 +0300

    Adds refresh of short term view to BPC app progressive loading

--- static/framework/bp_centiles/js/bpc-data.js
+++ static/framework/bp_centiles/js/bpc-data.js
@@ -198,6 +198,7 @@ if (!BPC) {
                     var patient = BPC.processData(demographics, vitals);
                     BPC.initPatient (patient);
                     BPC.patient = patient;
+                    BPC.redrawViewShort (BPC.patient,BPC.settings.zones);
                     BPC.setDateRange(0,100);
                     BPC.redrawViewLong (BPC.patient,BPC.settings.zones);
                     BPC.redrawViewTable (BPC.patient);
